865b8b4517ff0287276e06924566ebe8addfc241
qq61538187 <61538187@qq.com>
2021-02-09 22:23:48 +0800

    11

--- src/utils/icu/index.js
+++ src/utils/icu/index.js
@@ -1,9 +1,11 @@
 import mixin from './mixins/VueMixin'
 import request from './libs/request'
+import scrollAnimation from './libs/function/scrollAnimation'
 // filters
 import './filters'
 const $icu = {
-  request
+  request,
+  scrollAnimation
 }
 
 const install = Vue => {
